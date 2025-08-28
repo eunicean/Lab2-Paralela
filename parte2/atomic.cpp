425140b171f86c6cbd1d0a7d438d9971ccaeb02f
eunicean <eunice.mata2992@gmail.com>
2025-08-27 18:03:08 -0600

    atomic

---
+++ parte2/atomic.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#define N 100000000
+
+int main(int argc, char const *argv[])
+{
+    double *myarray = (double*) malloc(N * sizeof(double));
+
+    for (long i = 0; i < N; i++) {
+        myarray[i] = 1.0;   
+    }
+
+    double suma = 0.0;
+    double start = omp_get_wtime();
+    
+    #pragma omp parallel for
+    for (long i = 0; i < N; i++) {
+        #pragma omp atomic
+        suma += myarray[i];
+    }
+
+    double end = omp_get_wtime();
+    printf("Critical - Suma: %.0f, Tiempo: %f s\n", suma, end - start);
+    return 0;
+}
